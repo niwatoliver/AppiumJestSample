7189d996daae4aa717ca321d36ff4a0bec9c6f86
minakawa-daiki <minakawa.daiki.as@gmail.com>
2019-12-19 03:19:06 +0900

    Add android base tests and refactor

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,30 @@
+const iosEnvironmentOptions = {
+  port: 4723,
+  capabilities: {
+    platformName: "iOS",
+    platformVersion: "11.3",
+    deviceName: "iPhone X",
+    automationName: "XCUITest",
+    wdaLocalPort: 8100,
+    app: "./app/AppiumJestSample.app"
+  }
+};
+
+const androidEnvironmentOptions = {
+  port: 4723,
+  capabilities: {
+    platformName: "Android",
+    deviceName: "Android Emulator",
+    automationName: "Appium",
+    avd: "Pixel_3_API_29",
+    systemPort: 8200,
+    app: "./app/AppiumJestSample.apk"
+  }
+};
+
 module.exports = {
   testEnvironment: "jest-environment-webdriverio",
-  testEnvironmentOptions: {
-    port: 4723,
-    capabilities: {
-      platformName: "iOS",
-      platformVersion: "11.3",
-      deviceName: "iPhone X",
-      automationName: "XCUITest",
-      wdaLocalPort: 8100,
-      app: "./app/AppiumJestSample.app"
-    }
-  },
+  testEnvironmentOptions: iosEnvironmentOptions,
   moduleFileExtensions: [
     "ts",
     "js"
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,22 +1,41 @@
-import * as _ from 'webdriverio';
+import 'webdriverio';
 
-// TODO PCスペックに応じて、ここの数値を調整
+// PCスペックに応じて、ここの数値を調整
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
 
+const platformName = browser.capabilities.platformName;
+
 describe('AppiumJestSampleアプリのテスト', () => {
-  // 全体で一度だけの前処理
-  beforeAll(async () => await browser.pause(1000));
   // テストごとの前処理
-  afterEach(async () => await browser.pause(1000));
+  beforeEach(async () => await browser.pause(1000));
 
   test('test', async () => {
     const textInputElement = await browser.$('~TextInput');
     await textInputElement.addValue('TEST!!!!');
+    await browser.hideKeyboard();
+    await browser.pause(2000);
+
+    await swipe('left');
     await browser.pause(1000);
-    await textInputElement.addValue('Enter');
-    await browser.pause(1000);
-    await browser.execute('mobile: swipe', { direction: 'left' });
-    await browser.execute('mobile: swipe', { direction: 'left' });
+    await swipe('right');
+
     await browser.pause(5000);
   });
 });
+
+async function swipe(direction: 'left' | 'right') {
+  if(platformName === 'iOS') {
+    await browser.execute('mobile: swipe', { direction });
+  } else if(platformName === 'Android') {
+    const rootElement = await browser.$('//*');
+    const x = direction === 'left' ? -1000 : 1000;
+    await browser.touchFlick(x, 0, rootElement.elementId, 100);
+
+    // 現状ReactNativeはこれだとSwipeできない？（AndroidのNativeだと動くことを確認）
+    // await browser.touchPerform([
+    //   { action: 'press', options: { x: 1000, y: windowSize.height / 2 } },
+    //   { action: 'moveTo', options: { x: 100, y: windowSize.height / 2 } },
+    //   { action: 'release' },
+    // ]);
+  }
+}
